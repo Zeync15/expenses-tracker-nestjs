74c8f0a1a0d68231caccb0b9ef916b95e85b152b
Ze Yen <zeyenchai@gmail.com>
2023-07-22 16:04:15 +0800

    Change hashing method

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,12 +4,12 @@ import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly configService: ConfigService) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get("AUTH_SECRET"),
+      secretOrKey: configService.get("JWT_TOKEN_SECRET"),
     });
   }
 
